1076f9e0c66e7a6c72e327b392636d511f837305
Xander Dunn <xander@xander.ai>
2023-02-09 11:43:21 -0500

    Working backprop across a split and concat in the forward pass

--- src/numpy_testing.py
+++ src/numpy_testing.py
@@ -34,34 +34,43 @@ class NumpyNN():
 
     def forward(self, x: np.ndarray) -> np.ndarray:
         Z = np.matmul(x, self.W)
-        A = gelu(Z)
-        # A = relu(Z)
+        z1, z2 = np.split(Z, 2, axis=1)
+        # A = z1 * gelu(z2)
+        A = np.concatenate((z1, gelu(z2)), axis=1)
         return A
 
-    def backward(self, x: np.ndarray, dA: np.ndarray) -> np.ndarray:
+    def backward(self, x: np.ndarray, # [2, 2]
+                 dA: np.ndarray # [2, 4]
+                ) -> np.ndarray:
         """
         Backward pass of the linear layer with partial gelu activation.
         This returns the gradient of the linear layer's weights.
         """
-        Z = np.matmul(x, self.W)
-        dZ = dA * gelu_prime(Z)
-        # dZ = dA * relu_prime(Z)
-        dW = np.dot(dZ.T, x)
+        assert x.shape == (2, 2), f"Expected x to be [2, 2], got {x.shape}"
+        assert dA.shape == (2, 4), f"Expected dA to be [2, 2], got {dA.shape}"
+        dW1 = np.split(np.matmul(dA.T, x).T, 2, axis=1)[0] # [2, 2]
+
+        Z = np.matmul(x, self.W) # [2, 4]
+        _, z2 = np.split(Z, 2, axis=1) # [2, 2] and [2, 2]
+        _, dA2 = np.split(dA, 2, axis=1) # [2, 2] and [2, 2]
+        dz2 = dA2 * gelu_prime(z2) # [2, 2]
+        dW2 = np.matmul(dz2.T, x).T # [2, 2]
+
+        dW = np.concatenate((dW1, dW2), axis=1) # (2, 4)
+        assert dW.shape == self.W.shape, f"Expected dW to be {self.W.shape}, got {dW.shape}"
         return dW
 
 
 class TorchNN():
 
     def __init__(self, d_model: int):
-        # self.W = W
         self.linear = torch.nn.Linear(d_model, d_model * 2, bias=False)
         self.gelu = torch.nn.GELU()
-        # self.relu = torch.nn.ReLU()
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         Z = self.linear(x)
-        A = self.gelu(Z)
-        # A = self.relu(Z)
+        z1, z2 = torch.chunk(Z, 2, dim=1)
+        A = torch.cat((z1, self.gelu(z2)), 1)
         return A
 
 
@@ -69,24 +78,27 @@ def main():
     torch.manual_seed(0)
     np.random.seed(0)
 
-    d_model = 16
-    batch_size = 16
+    d_model = 2
+    batch_size = 2
     x = torch.randn(batch_size, d_model, device='cpu')  # input
-    # W = torch.randn(d_model, d_model, device='cpu', requires_grad=True) # weights
 
     # PyTorch
     torch_nn = TorchNN(d_model)
     torch_forward = torch_nn.forward(x)
-    grad = torch.randn(batch_size, d_model * 2, device='cpu')
+    # This is dA, or some upstream gradient that we must backpropagate
+    grad = torch.ones(batch_size, d_model * 2, device='cpu')
     torch_forward.backward(grad)
     torch_grad = torch_nn.linear.weight.grad
+    assert torch_grad is not None
+    torch_grad = torch_grad.T
 
     # Numpy
     W = torch_nn.linear.state_dict()["weight"].T
-    # W = torch_nn.linear.state_dict()["weight"]
+    assert torch_grad.shape == W.shape
     numpy_nn = NumpyNN(W.detach().numpy())
     numpy_forward = numpy_nn.forward(x.detach().numpy())
     numpy_grad = numpy_nn.backward(x.detach().numpy(), grad.detach().numpy())
+    assert numpy_grad.shape == W.shape
 
     triton.testing.assert_almost_equal(torch_forward.detach().numpy(), numpy_forward)
     triton.testing.assert_almost_equal(torch_grad, numpy_grad)
