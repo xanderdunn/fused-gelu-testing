705f2e973b259c8041cba8fefe7c8958e69d4077
Xander Dunn <xander@xander.ai>
2023-02-09 15:29:44 -0500

    Reformat the triton code to incldue the backward stub

--- src/fused_gelu.py
+++ src/fused_gelu.py
@@ -5,26 +5,26 @@ import triton
 import triton.language as tl
 
 @triton.jit
-def matmul_chunk_gelu(
+def gelu_partial_layer_fused_forward(
         # Pointers to matrices
-        a_ptr, b_ptr, c_ptr,
+        x_ptr, W_ptr, A_ptr, z1_ptr, z2_ptr,
         # Matrix dimensions
         M, N, K,
         # The stride variables represent how much to increase the ptr by when moving by 1
-        # element in a particular dimension. E.g. stride_am is how much to increase a_ptr
+        # element in a particular dimension. E.g. stride_xm is how much to increase x_ptr
         # by to get the element one row down (A has M rows)
-        stride_am, stride_ak,
-        stride_bk, stride_bn,
-        stride_cm, stride_cn,
+        stride_xm, stride_xk,
+        stride_Wk, stride_Wn,
+        stride_Am, stride_An,
         # Meta-parameters
         BLOCK_SIZE_M: tl.constexpr, BLOCK_SIZE_N: tl.constexpr, BLOCK_SIZE_K: tl.constexpr,
         GROUP_SIZE_M: tl.constexpr,
         ):
     """
-    1) Compute matmul C = A x B into two halves, X1 and X2
-    2) Compute C = X1 * gelu(X2)
-    3) Return C
-    A has shape (M, K), B has shape (K, N) and C has shape (M, N//2)
+    1) Compute Z = matmul(x, W) into two halves, Z1 and Z2
+    2) Compute element-wise multiply A = Z1 * gelu(Z2), this is the activation
+    3) Return A
+    x has shape (M, K), W has shape (K, N) and A has shape (M, N//2)
     """
     # L2 Cache Optimizations
     pid = tl.program_id(axis=0)
@@ -41,91 +41,131 @@ def matmul_chunk_gelu(
     offs_am = pid_m * BLOCK_SIZE_M + tl.arange(0, BLOCK_SIZE_M)
     offs_bn = pid_n * BLOCK_SIZE_N + tl.arange(0, BLOCK_SIZE_N)
     offs_k = tl.arange(0, BLOCK_SIZE_K)
-    a_ptrs = a_ptr + (offs_am[:, None] * stride_am + offs_k[None, :] * stride_ak)
-    b_left_ptrs = b_ptr + (offs_k[:, None] * stride_bk + offs_bn[None, :] * stride_bn)
+    x_ptrs = x_ptr + (offs_am[:, None] * stride_xm + offs_k[None, :] * stride_xk)
+    W_left_ptrs = W_ptr + (offs_k[:, None] * stride_Wk + offs_bn[None, :] * stride_Wn)
     # offset the start by half of the number of columns
-    b_right_ptrs = b_ptr + (N // 2) + (offs_k[:, None] * stride_bk + offs_bn[None, :] * stride_bn)
+    W_right_ptrs = W_ptr + (N // 2) + (offs_k[:, None] * stride_Wk + offs_bn[None, :] * stride_Wn)
 
-    x1 = tl.zeros((BLOCK_SIZE_M, BLOCK_SIZE_N), dtype=tl.float32)
-    x2 = tl.zeros((BLOCK_SIZE_M, BLOCK_SIZE_N), dtype=tl.float32)
+    z1 = tl.zeros((BLOCK_SIZE_M, BLOCK_SIZE_N), dtype=tl.float32)
+    z2 = tl.zeros((BLOCK_SIZE_M, BLOCK_SIZE_N), dtype=tl.float32)
     # Matrix multiply A and B, accumulating the first half of columns into x1 and
     # the second half of columns into x2
     for _ in range(0, K, BLOCK_SIZE_K): # type: ignore
-        a = tl.load(a_ptrs)
+        a = tl.load(x_ptrs)
 
-        b_left = tl.load(b_left_ptrs)
-        x1 += tl.dot(a, b_left)
+        W_left = tl.load(W_left_ptrs)
+        z1 += tl.dot(a, W_left)
 
-        b_right = tl.load(b_right_ptrs)
-        x2 += tl.dot(a, b_right)
+        W_right = tl.load(W_right_ptrs)
+        z2 += tl.dot(a, W_right)
 
         # Advance the ptrs to the next K block
-        a_ptrs += BLOCK_SIZE_K * stride_ak
-        b_left_ptrs += BLOCK_SIZE_K * stride_bk
-        b_right_ptrs += BLOCK_SIZE_K * stride_bk
+        x_ptrs += BLOCK_SIZE_K * stride_xk
+        W_left_ptrs += BLOCK_SIZE_K * stride_Wk
+        W_right_ptrs += BLOCK_SIZE_K * stride_Wk
 
-    # Element-wise multiply of x1 and gelu(x2)
-    x2 = x1 * gelu_fast(x2)
+    # TODO: Store z1 in z1_ptr
+    # TODO: Store z2 in z2_ptr
 
-    c = x2.to(tl.float16)
+    # Element-wise multiply of z1 and gelu(z2)
+    z2 = gelu_fast(z2)
+    c = z1 * z2
+    c = c.to(tl.float16)
 
     # Write back the block of the output matrix C
     offs_cm = pid_m * BLOCK_SIZE_M + tl.arange(0, BLOCK_SIZE_M)
     offs_cn = pid_n * BLOCK_SIZE_N + tl.arange(0, BLOCK_SIZE_N)
-    c_ptrs = c_ptr + stride_cm * offs_cm[:, None] + stride_cn * offs_cn[None, :]
-    c_mask = (offs_cm[:, None] < M) & (offs_cn[None, :] < N // 2)
-    tl.store(c_ptrs, c, mask=c_mask)
+    A_ptrs = A_ptr + stride_Am * offs_cm[:, None] + stride_An * offs_cn[None, :]
+    A_mask = (offs_cm[:, None] < M) & (offs_cn[None, :] < N // 2)
+    tl.store(A_ptrs, c, mask=A_mask)
 
+@triton.jit
+def gelu_partial_layer_fused_backward(x_ptr, W_ptr, z1_ptr, z2_ptr, dA_ptr, dW_ptr):
+    # dA1 = dA * gelu(z2)
+    # dz1 = dA1 # there's no activation function on this half
+    # dW1 = np.matmul(dz1.T, x).T
+
+    # dA2 = dA * z1
+    # dz2 = dA2 * gelu_prime(z2)
+    # dW2 = np.matmul(dz2.T, x).T
+
+    # dW = np.concatenate((dW1, dW2), axis=1)
+
+    # Do a for-loop element-wise multiply and accumulate dA1 and dA2
+    # Do a for-loop matmul and accumulate dW1 and dW2
+    pass
 
 @triton.jit
 def gelu_fast(x):
-    # From https://pytorch.org/docs/stable/generated/torch.nn.GELU.html
-    # 0.5 * x * (1 + tanh(sqrt(2/pi)) * (x + 0.044715 * x**3))
-    # 0.5 * x * (1.0 + tl.libdevice.tanh(0.7978845608028654 * (x + 0.044715 * x * x * x)))
+    """
+    From https://pytorch.org/docs/stable/generated/torch.nn.GELU.html
+    0.5 * x * (1 + tanh(sqrt(2/pi)) * (x + 0.044715 * x**3))
+    0.5 * x * (1.0 + tl.libdevice.tanh(0.7978845608028654 * (x + 0.044715 * x * x * x)))
+    """
     return (
             0.5 * x * (1.0 + tl.libdevice.tanh(0.7978845608028654 * x * (1.0 + 0.044715 * x * x)))
            )
 
 @triton.jit
 def gelu_fast_prime(x):
-    # From https://arxiv.org/pdf/2104.02523.pdf
-    term1 = 0.0356774 * x * x * x + 0.797885 * x
-    denominator = tl.libdevice.cosh(term1)
-    return (
-            0.5 * tl.libdevice.tanh(term1) + 0.5 + (0.0535161 * x * x * x + 0.398942 * x) * (1.0 / (denominator * denominator))
-           )
+    """
+    From https://github.com/MarkTigchelaar/Tinman/blob/27a492c06105d550d7eacd2ca9fadc089d484c3a/src/neural_network_parts/activator.rs#L232
+    In my testing this is a close approximation but not exact match of the backward pass of the pytorch GELU()
+    """
+    term1 = 0.0356774 * x * x * x
+    term2 = 0.398942 * x
+    term3 = 0.797885 * x
+    term4 = 0.0535161  * x * x * x
+    hyp_secant = 1 / tl.libdevice.cosh(term1 + term3)
+    hyp_secant *= hyp_secant
+    return 0.5 * tl.libdevice.tanh(term1 + term3) + (term4 + term2) * hyp_secant + 0.5
 
 
-def matmul(a, b):
-    # checks constraints
-    assert a.shape[1] == b.shape[0], "incompatible dimensions"
-    assert a.is_contiguous(), "matrix A must be contiguous"
-    assert b.is_contiguous(), "matrix B must be contiguous"
-    M, K = a.shape
-    K, N = b.shape
-    assert (
-            K % 16 == 0
-            ), "We don't check memory-out-of-bounds with K so K must be divisible by BLOCK_SIZE_K"
-    # allocates output
-    c = torch.empty((M, N // 2), device=a.device, dtype=a.dtype)
-    # 1D launch kernel where each block gets its own program.
-    grid = lambda META: (
-            triton.cdiv(M, META['BLOCK_SIZE_M']) * triton.cdiv(N, META['BLOCK_SIZE_N']),
-            )
-    matmul_chunk_gelu[grid]( # type: ignore
-        a, b, c,
-        M, N, K,
-        a.stride(0), a.stride(1),
-        b.stride(0), b.stride(1),
-        c.stride(0), c.stride(1),
-        # TODO: In production we would not want to hardcode these, but likely want to
-        # find them via triton's autotuner.
-        BLOCK_SIZE_M=32, # type: ignore
-        BLOCK_SIZE_N=32, # type: ignore
-        BLOCK_SIZE_K=32, # type: ignore
-        GROUP_SIZE_M=8, # type: ignore
-    )
-    return c
+class PartialGeluLayer(torch.autograd.Function):
+
+    @staticmethod
+    def forward(ctx, x: torch.Tensor, W: torch.Tensor) -> torch.Tensor:
+        # checks constraints
+        assert x.shape[1] == W.shape[0], "incompatible dimensions"
+        assert x.is_contiguous(), "matrix A must be contiguous"
+        assert W.is_contiguous(), "matrix B must be contiguous"
+        M, K = x.shape
+        K, N = W.shape
+        assert (
+                K % 16 == 0
+                ), "We don't check memory-out-of-bounds with K so K must be divisible by BLOCK_SIZE_K"
+        # allocates output
+        A = torch.empty((M, N // 2), device=x.device, dtype=x.dtype)
+        z1 = torch.empty((M, N), device=x.device, dtype=x.dtype) # same dimensions as the weights
+        z2 = torch.empty((M, N), device=x.device, dtype=x.dtype)
+        # 1D launch kernel where each block gets its own program.
+        grid = lambda META: (
+                triton.cdiv(M, META['BLOCK_SIZE_M']) * triton.cdiv(N, META['BLOCK_SIZE_N']),
+                )
+        gelu_partial_layer_fused_forward[grid]( # type: ignore
+            x, W, A, z1, z2,
+            M, N, K,
+            x.stride(0), x.stride(1),
+            W.stride(0), W.stride(1),
+            A.stride(0), A.stride(1),
+            # TODO: In production we would not want to hardcode these, but likely want to
+            # find them via triton's autotuner.
+            BLOCK_SIZE_M=32, # type: ignore
+            BLOCK_SIZE_N=32, # type: ignore
+            BLOCK_SIZE_K=16, # type: ignore
+            GROUP_SIZE_M=8, # type: ignore
+        )
+        ctx.save_for_backward(x, W, z1, z2)
+        return A
+
+    def backward(ctx, dA: torch.Tensor) -> torch.Tensor:
+        x, W, z1, z2 = ctx.saved_tensors
+        dW = torch.empty(W.shape, device=x.device, dtype=x.dtype)
+        grid = lambda META: (
+                triton.cdiv(M, META['BLOCK_SIZE_M']) * triton.cdiv(N, META['BLOCK_SIZE_N']),
+                )
+        gelu_partial_layer_fused_backward[grid](x, W, z1, z2, dA, dW)
+        return dW
 
 
 class TorchNN():
@@ -140,12 +180,13 @@ class TorchNN():
         result = x1 * self.gelu(x2)
         return result
 
+partial_gelu = PartialGeluLayer.apply
 
 def main():
     print("Doing a forward pass: Linear layer -> split in two by column -> GELU on the second half -> element-wise multiply of the first and second halves")
     print("In both pytorch and triton to compare correctness...")
-    batch_size = 512 
-    d_model = 512 
+    batch_size = 512
+    d_model = 512
     torch.manual_seed(0)
     # input to feed forward
     x = torch.randn((batch_size, d_model), device='cuda', dtype=torch.float16)
@@ -155,13 +196,13 @@ def main():
 
     linear_weights = torch_nn.linear.state_dict()["weight"].T.contiguous()
     # biases = torch_nn.linear.state_dict()["bias"]
-    triton_forward = matmul(x, linear_weights)
+    triton_forward = partial_gelu(x, linear_weights)
     print(f"torch_forward={torch_forward}")
     print(f"triton_forward={triton_forward}")
     if triton.testing.allclose(triton_forward, torch_forward):
-        print("✅ Triton and Torch match")
+        print("Forward: ✅ Triton and Torch match")
     else:
-        print("❌ Triton and Torch differ")
+        print("Forward: ❌ Triton and Torch differ")
     # TODO: Benchmark the two implementations across increasing sizes of matrices
 
 
--- src/numpy_testing.py
+++ src/numpy_testing.py
@@ -5,6 +5,14 @@ import numpy as np
 import triton
 
 
+# Useful pieces:
+# https://towardsdatascience.com/lets-code-a-neural-network-in-plain-numpy-ae7e74410795
+# https://medium.com/analytics-vidhya/neural-network-mnist-classifier-from-scratch-using-numpy-library-94bbcfed7eae
+# https://cs231n.github.io/optimization-2/
+# https://stackoverflow.com/questions/57248777/backward-function-in-pytorch
+# https://towardsdatascience.com/gradient-descent-from-scratch-e8b75fa986cc
+# https://goodboychan.github.io/python/datacamp/deep_learning/2020/07/21/02-Optimizing-a-neural-network-with-backward-propagation.html
+
 def relu(x):
     return np.maximum(0, x)
 
@@ -49,6 +57,7 @@ class NumpyNN():
         Backward pass of the linear layer with partial gelu activation.
         This returns the gradient of the linear layer's weights.
         """
+        # This is a duplicated matmul and would instead ideally be gotten from storage elsewhere
         Z = np.matmul(x, self.W)
         z1, z2 = np.split(Z, 2, axis=1)
 
